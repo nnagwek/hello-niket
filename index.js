4dae6de705aaa54655422777b1f48535c722f040
Niket Nagwekar <Niket.Nagwekar@morningstar.com>
2020-11-19 14:44:34 +0530

    bet version 0

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-console.log("testing index.js................");
-console.log("Hello Niket !!!................");
+module.exports  = function(lang){
+    if( lang== "es"){
+        console.log("testiing index.js................");
+        console.log("Hello  !!!................");
+    } else {
+        console.log("testing index.js................");
+        console.log("Hello Niket !!!................");
+    }
+}
